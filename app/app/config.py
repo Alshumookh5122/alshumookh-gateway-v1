dfa9c0c77e53ce8d5836f73ec088b72a5a2abe42
Alshumookh5122 <remooz.cbl@gmail.com>
2026-04-16 23:54:26 +0400

    Modify settings in config.py
    Updated APP_NAME to include '&' and set case sensitivity to False.

--- app/app/config.py
+++ app/app/config.py
@@ -5,22 +5,13 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=True,
+        case_sensitive=False,
         extra="ignore",
     )
 
-    APP_NAME: str = "ALSHUMOOKH GLOBAL BANKING FINANCE AND CREDIT"
-    APP_VERSION: str = "1.0.0"
-
+    APP_NAME: str = "ALSHUMOOKH GLOBAL BANKING FINANCE & CREDIT"
     ALCHEMY_API_KEY: str = ""
-    ALCHEMY_CHAIN: str = "TRON"
-    ALCHEMY_WEBHOOK_SECRET: str = ""
-
-    DEFAULT_WALLET_ADDRESS: str = ""
-    DEFAULT_TOKEN_SYMBOL: str = "USDT"
-    DEFAULT_NETWORK: str = "TRON"
-    USDT_CONTRACT: str = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
-    MIN_CONFIRMATIONS: int = 1
+    WALLET_ADDRESS: str = ""
 
 
 settings = Settings()
